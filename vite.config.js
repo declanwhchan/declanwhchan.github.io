50a8c81da7b9008e1dd5bbe48cce6658c8511d24
declanwhchan <declanwhchan@users.noreply.github.com>
2026-07-18 23:06:02 +0000

    Deploying to gh-pages from @ declanwhchan/declanwhchan.github.io@f000de47c208301f3fa5c43af16b86122c39595a 🚀

---
+++ vite.config.js
@@ -0,0 +1,1 @@
+import{defineConfig}from"vite";import react from"@vitejs/plugin-react";export default defineConfig({base:"/",plugins:[react()],build:{sourcemap:!1,rollupOptions:{output:{entryFileNames:"assets/[name].js",chunkFileNames:"assets/[name].js",assetFileNames:"assets/[name][extname]"}}}});
